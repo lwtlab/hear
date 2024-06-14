6a337a451cd62ac4a4408edba55b225d91171ebd
jiahua <littleguner@gmail.com>
2024-06-14 16:19:33 +0800

    clarity 分析

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,13 @@ export default defineConfig({
   integrations: [
     starlight({
       favicon: "/favicon.ico",
+      head:[
+        {
+          // https://clarity.microsoft.com/projects/view/mrq0ce2b1x/gettingstarted
+          tag:"script",
+          content:'(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "mrq0ce2b1x");'
+        }
+      ],
       plugins: [starlightImageZoom()],
       components: {
         Header: "./src/components/LwtHeader.astro",
